de80b8d483e2d85ad1be8adc6dc730a116bd30f9
noomenehajer <hajer.noomene@gmail.com>
2023-03-08 18:48:38 +0100

    login admin frontend

---
+++ frontend/angular/src/app/services/admin.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  private apiUrl = 'http://localhost:3000/admin'; // replace with your Node.js server URL
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+
+  changePassword(email: string, oldPassword: string, newPassword: string): Observable<any> {
+    return this.http.put(`${this.apiUrl}/change-password`, { email, oldPassword, newPassword });
+  }
+}
--- frontend/angular/src/app/views/auth/loginadmin/loginadmin.component.ts
+++ frontend/angular/src/app/views/auth/loginadmin/loginadmin.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../../services/admin.service';
 
 @Component({
   selector: 'app-loginadmin',
   templateUrl: './loginadmin.component.html',
   styleUrls: ['./loginadmin.component.css']
 })
-export class LoginadminComponent {
 
+export class LoginadminComponent {
+  email: string;
+  password: string;
+  
+  constructor(private adminService: AdminService) {}
+  
+  onSubmit(): void {
+     this.adminService.login(this.email, this.password)
+        .subscribe(response => {
+          const token = response.token; // store the JWT token in localStorage or a cookie
+          // redirect to the admin dashboard or some other protected route
+        }, error => {
+          console.error(error);
+          // display an error message to the user
+        });
+    }
+  
 }
